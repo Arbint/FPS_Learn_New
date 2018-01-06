f24d504291c895c1c9f18942b65d53439e31d3ee
Jingtian Li <jingtianli.animation@gmail.com>
2018-01-05 20:27:55 -0600

    Decal Refined for ExtractionZone

---
+++ Source/MultIplayerShooter/Private/ExtractionZone.cpp
@@ -0,0 +1,36 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "ExtractionZone.h"
+#include "Components/BoxComponent.h"
+#include "Components/DecalComponent.h"
+#include "MultIplayerShooterCharacter.h"
+// Sets default values
+AExtractionZone::AExtractionZone()
+{
+	OverlapComp = CreateDefaultSubobject<UBoxComponent>(TEXT("OverlayComp"));
+	OverlapComp->SetCollisionEnabled(ECollisionEnabled::QueryOnly);
+	OverlapComp->SetCollisionResponseToAllChannels(ECR_Ignore);
+	OverlapComp->SetCollisionResponseToChannel(ECC_Pawn, ECR_Overlap);
+	OverlapComp->SetBoxExtent(FVector(200.0f));
+	DecalComp = CreateDefaultSubobject<UDecalComponent>(TEXT("Decal Component"));
+	DecalComp->SetupAttachment(RootComponent);
+	DecalComp->DecalSize = FVector(200.0f, 200.0f, 200.0f);
+	SetRootComponent(OverlapComp);
+
+	OverlapComp->OnComponentBeginOverlap.AddDynamic(this, &AExtractionZone::HandleBeginOverlap);
+	OverlapComp->SetHiddenInGame(false);
+
+}
+
+void AExtractionZone::HandleBeginOverlap(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool bFromSweep, const FHitResult& SweepResult)
+{
+	AMultIplayerShooterCharacter* PlayerCharacter = Cast<AMultIplayerShooterCharacter>(OtherActor);
+	if (ensure(PlayerCharacter))
+	{
+		if (PlayerCharacter->HasObjective())
+		{
+
+		}
+	}
+}
+
---
+++ Source/MultIplayerShooter/Public/ExtractionZone.h
@@ -0,0 +1,25 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Actor.h"
+#include "ExtractionZone.generated.h"
+class UBoxComponent;
+class UDecalComponent;
+UCLASS()
+class MULTIPLAYERSHOOTER_API AExtractionZone : public AActor
+{
+	GENERATED_BODY()
+	
+public:	
+	// Sets default values for this actor's properties
+	AExtractionZone();
+public:	
+	UPROPERTY(VisibleAnywhere, Category = "Function")
+	UBoxComponent* OverlapComp;
+	UPROPERTY(VisibleAnywhere, Category = "Function")
+	UDecalComponent* DecalComp;
+	UFUNCTION()
+	void HandleBeginOverlap(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool bFromSweep, const FHitResult& SweepResult);
+};
